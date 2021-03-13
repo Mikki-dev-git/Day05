915ea6b31c20c6b4a289630c223ccb3b0f030c4c
Mikki-dev-git <73258978+Mikki-dev-git@users.noreply.github.com>
2021-03-13 10:45:38 +0100

    commit

---
+++ ex08/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+  // Only change code below this line
+  var lastNameLenght = "Doe".length;
+  // Only change code above this line
+  return lastNameLenght;
+}
+
+console.log(main());
+module.exports = main;
